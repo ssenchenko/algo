13e0af0bb303b8bfe2d49a39d51008722e07c08e
Slava Senchenko <ssenchenko@gmail.com>
2020-06-23 23:04:09 -0400

    uf

---
+++ adt/test_UnionFind.cpp
@@ -0,0 +1,24 @@
+#include "./UnionFind.h"
+
+#include <gtest/gtest.h>
+
+using namespace algo;
+
+TEST(UnionFind, AddAndTestDirectConnection)
+{
+    auto uf = algo::UnionFind<int>();
+    uf.Connect({ 3,5 });
+    EXPECT_TRUE(uf.AreConnected({ 3,5 }));
+}
+
+TEST(UnionFind, AddAndTestIndirectConnection)
+{
+    auto uf = algo::UnionFind<int>();
+    uf.Connect({ 1, 2 });
+    uf.Connect({ 2, 3 });
+    uf.Connect({ 1, 4 });
+    uf.Connect({ 3, 5 });
+    auto connected = uf.AreConnected({ 1, 5 });
+    EXPECT_TRUE(connected);
+    EXPECT_FALSE(uf.AreConnected({ 1, 7 }));
+}
