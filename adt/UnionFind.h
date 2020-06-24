ec9b97fe54b2ce00cbc8df69d33f2bc4a891c91d
Slava Senchenko <ssenchenko@gmail.com>
2020-06-24 01:22:06 -0400

    format

--- adt/UnionFind.h
+++ adt/UnionFind.h
@@ -4,59 +4,49 @@
 #include <unordered_set>
 #include <utility>
 
-namespace algo
-{
+namespace algo {
 
 template <class T>
-class UnionFind
-{
-private:
-    std::unordered_map<T, std::unordered_set<T>> items_;
+class UnionFind {
+ private:
+  std::unordered_map<T, std::unordered_set<T>> items_;
 
-public:
-    UnionFind() {};
-    void Connect(std::pair<T, T> connected);
-    void Disconnect(std::pair<T, T> connected);
-    bool AreConnected(std::pair<T, T> points);
+ public:
+  UnionFind(){};
+  void Connect(std::pair<T, T> connected);
+  void Disconnect(std::pair<T, T> connected);
+  bool AreConnected(std::pair<T, T> points);
 };
 
+}  // namespace algo
+
+template <class T>
+void algo::UnionFind<T>::Connect(std::pair<T, T> connected) {
+  if (items_.contains(connected.first)) {
+    items_.insert({connected.first, std::unordered_set<T>()});
+  }
+  items_[connected.first].insert(connected.second);
+  if (items_.contains(connected.second)) {
+    items_.insert({connected.second, std::unordered_set<T>()});
+  }
+  items_[connected.second].insert(connected.first);
 }
 
 template <class T>
-void algo::UnionFind<T>::Connect(std::pair<T, T> connected)
-{
-    if (items_.find(connected.first) == items_.end()) 
-    {
-        items_.insert({ connected.first, std::unordered_set<T>() });
-    }
-    items_[connected.first].insert(connected.second);
-    if (items_.find(connected.second) == items_.end())
-    {
-        items_.insert({ connected.second, std::unordered_set<T>() });
-    }
-    items_[connected.second].insert(connected.first);
+void algo::UnionFind<T>::Disconnect(std::pair<T, T> connected) {
+  items_[connected.first].erase(connected.second);
+  items_[connected.second].erase(connected.first);
 }
 
 template <class T>
-void algo::UnionFind<T>::Disconnect(std::pair<T, T> connected)
-{
-    items_[connected.first].erase(connected.second);
-    items_[connected.second].erase(connected.first);
-}
-
-template <class T>
-bool algo::UnionFind<T>::AreConnected(std::pair<T, T> points)
-{
-    if (items_.count(points.first))
-    {
-        if (items_[points.first].find(points.second) != items_[points.first].end())
-        {
-            return true;
-        }
-        for (const auto& item : items_[points.first])
-        {
-            if (AreConnected({ item, points.second })) return true; // O(c^N) :(
-        }
+bool algo::UnionFind<T>::AreConnected(std::pair<T, T> points) {
+  if (items_.count(points.first)) {
+    if (items_[points.first].find(points.second) != items_[points.first].end()) {
+      return true;
     }
-    return false;
+    for (const auto &item : items_[points.first]) {
+      if (AreConnected({item, points.second})) return true;  // O(c^N) :(
+    }
+  }
+  return false;
 }
--- adt/test_UnionFind.cpp
+++ adt/test_UnionFind.cpp
@@ -1,24 +1,22 @@
-#include "./UnionFind.h"
-
 #include <gtest/gtest.h>
+
+#include "./UnionFind.h"
 
 using namespace algo;
 
-TEST(UnionFind, AddAndTestDirectConnection)
-{
-    auto uf = algo::UnionFind<int>();
-    uf.Connect({ 3,5 });
-    EXPECT_TRUE(uf.AreConnected({ 3,5 }));
+TEST(UnionFind, AddAndTestDirectConnection) {
+  auto uf = algo::UnionFind<int>();
+  uf.Connect({3, 5});
+  EXPECT_TRUE(uf.AreConnected({3, 5}));
 }
 
-TEST(UnionFind, AddAndTestIndirectConnection)
-{
-    auto uf = algo::UnionFind<int>();
-    uf.Connect({ 1, 2 });
-    uf.Connect({ 2, 3 });
-    uf.Connect({ 1, 4 });
-    uf.Connect({ 3, 5 });
-    auto connected = uf.AreConnected({ 1, 5 });
-    EXPECT_TRUE(connected);
-    EXPECT_FALSE(uf.AreConnected({ 1, 7 }));
+TEST(UnionFind, AddAndTestIndirectConnection) {
+  auto uf = algo::UnionFind<int>();
+  uf.Connect({1, 2});
+  uf.Connect({2, 3});
+  uf.Connect({1, 4});
+  uf.Connect({3, 5});
+  auto connected = uf.AreConnected({1, 5});
+  EXPECT_TRUE(connected);
+  EXPECT_FALSE(uf.AreConnected({1, 7}));
 }
