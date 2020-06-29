67c7cc1f769901e414769fddab89f79798b2a6e6
Slava Senchenko <ssenchenko@gmail.com>
2020-06-28 23:22:53 -0400

    has all unique

--- challenges/arrays.cpp
+++ challenges/arrays.cpp
@@ -1,5 +1,7 @@
 #include "arrays.hpp"
 
+#include <algorithm>
+#include <iterator>
 #include <set>
 
 bool chl::HasAllUnique(std::string_view str) {
@@ -17,5 +19,13 @@ bool chl::HasAllUnique2(std::string_view str) {
   // Time O(N^2)
   // Memory O(1)
 
-  return false;
+  // only to be sure that iterator arithmetics won't create a bug for an empty string case
+  if (std::empty(str)) return true;
+
+  auto after_last = std::end(str);
+  for (auto it = std::begin(str), last = std::prev(after_last); it < last; ++it) {
+    // if symbol at <it> position can be found in [it+1, end_of_string] -> not all unique symbols
+    if (std::find(std::next(it), after_last, *it) != after_last) return false;
+  }
+  return true;
 }
--- challenges/arrays_tests.cpp
+++ challenges/arrays_tests.cpp
@@ -1,17 +1,21 @@
-#include "arrays.hpp"
-
 #include <gtest/gtest.h>
 
+#include "arrays.hpp"
 
-TEST(Has_All_Unique, EmptyString) {
-  EXPECT_TRUE(chl::HasAllUnique(""));
-}
+TEST(Has_All_Unique, EmptyString) { EXPECT_TRUE(chl::HasAllUnique("")); }
 
 TEST(Has_All_Unique, AllUnique) {
   EXPECT_TRUE(chl::HasAllUnique("2"));
   EXPECT_TRUE(chl::HasAllUnique("2dfg$Sp[D"));
 }
 
-TEST(Has_All_Unique, NotUnique) {
-  EXPECT_FALSE(chl::HasAllUnique("2dfg$Sp[d"));
+TEST(Has_All_Unique, NotUnique) { EXPECT_FALSE(chl::HasAllUnique("2dfg$Sp[d")); }
+
+TEST(Has_All_Unique2, EmptyString) { EXPECT_TRUE(chl::HasAllUnique2("")); }
+
+TEST(Has_All_Unique2, AllUnique) {
+  EXPECT_TRUE(chl::HasAllUnique2("2"));
+  EXPECT_TRUE(chl::HasAllUnique2("2dfg$Sp[D"));
 }
+
+TEST(Has_All_Unique2, NotUnique) { EXPECT_FALSE(chl::HasAllUnique2("2dfg$Sp[d")); }
